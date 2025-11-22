e1eb632e0ee3d3bc7874934a46b5c085335dc7a3
zjf <zhongfjinfu@moren.com>
2025-11-22 09:03:11 +0800

    refactor: Centralize Sass variable imports using `next.config.js` `additionalData` to remove individual `@use` statements.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(process.cwd(), 'src/styles')],
+    additionalData: `@use "${path.join(process.cwd(), 'src/styles/variables')}" as *;`,
   },
 };
 
